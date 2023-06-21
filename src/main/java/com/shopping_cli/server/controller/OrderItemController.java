cb33de28001894d603a9b99ee50c830081f8257c
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-21 19:09:23 +0300

    Bug fix inside OrderItemController.

--- src/main/java/com/shopping_cli/server/controller/OrderItemController.java
+++ src/main/java/com/shopping_cli/server/controller/OrderItemController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/order-items")
 @CrossOrigin
 public class OrderItemController {
     @Autowired
