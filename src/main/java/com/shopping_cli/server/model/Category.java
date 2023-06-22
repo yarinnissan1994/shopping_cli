1a0333629456051da7c4210778fdf3ed162f1ab8
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-22 15:14:32 +0300

    Added category model.

---
+++ src/main/java/com/shopping_cli/server/model/Category.java
@@ -0,0 +1,37 @@
+package com.shopping_cli.server.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", nullable = false, updatable = false)
+    private int id;
+    @Column(name = "user_name", nullable = false)
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
