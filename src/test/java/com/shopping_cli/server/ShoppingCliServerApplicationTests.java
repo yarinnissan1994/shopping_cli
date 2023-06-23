ca1dbcbf924f5a4c621df3e62f3141badf9a23ee
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-24 01:54:40 +0300

    commented out unnecessary test.

--- src/test/java/com/shopping_cli/server/ShoppingCliServerApplicationTests.java
+++ src/test/java/com/shopping_cli/server/ShoppingCliServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShoppingCliServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
