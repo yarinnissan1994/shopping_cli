53df576fd722aa5175aa740f4f8321be24628618
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-24 07:50:33 +0300

    Refactor User layers to work with session and make simple authorization system.

--- src/main/java/com/shopping_cli/server/controller/UserController.java
+++ src/main/java/com/shopping_cli/server/controller/UserController.java
@@ -2,8 +2,10 @@ package com.shopping_cli.server.controller;
 
 import com.shopping_cli.server.model.User;
 import com.shopping_cli.server.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -27,10 +29,16 @@ public class UserController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,  "User not found!"));
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        if (userService.existsByEmail(user)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("User already exists");
+        }
+
         userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("User created successfully");
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -51,5 +59,37 @@ public class UserController {
             userService.deleteById(id);
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user, HttpSession session) {
+        if (userService.existsByEmail(user)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("User already exists");
+        }
 
+        userService.register(session, user);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("User created successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user, HttpSession session) {
+        boolean userFound = userService.login(user.getEmail(), user.getPassword(),  session);
+        if (!userFound)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Incorrect information");
+        else
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Successfully logged in");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpSession session) {
+        session.invalidate();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Successfully logged out");
+    }
+
+    @GetMapping("/current")
+    public User getCurrentUser(HttpSession session) {
+        return userService.getCurrentUser(session);
+    }
 }
--- src/main/java/com/shopping_cli/server/service/UserService.java
+++ src/main/java/com/shopping_cli/server/service/UserService.java
@@ -2,6 +2,7 @@ package com.shopping_cli.server.service;
 
 import com.shopping_cli.server.model.User;
 import com.shopping_cli.server.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepo;
+    private static final String USER_SESSION_ATTRIBUTE = "user";
 
     public List<User> findAll(){
         return userRepo.findAll();
@@ -31,5 +33,39 @@ public class UserService {
 
     public void deleteById(int id) {
         userRepo.deleteById(id);
+    }
+
+    public boolean existsByEmail(User user) {
+        return userRepo.existsByEmail(user.getEmail());
+    }
+
+    public boolean register(HttpSession session, User user) {
+        if(userRepo.existsByEmail(user.getEmail())) return false;
+
+        userRepo.save(user);
+
+        return login(user.getEmail(), user.getPassword(), session);
+    }
+
+    public boolean login(String email, String password, HttpSession session) {
+        User user = userRepo.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            getOrCreateUserSession(session, email);
+            return true;
+        }
+        return false;
+    }
+
+    public User getCurrentUser(HttpSession session) {
+        return getOrCreateUserSession(session, "");
+    }
+
+    private User getOrCreateUserSession(HttpSession session, String userEmail) {
+        User user = (User) session.getAttribute(USER_SESSION_ATTRIBUTE);
+        if (user == null) {
+            user = userRepo.findByEmail(userEmail);
+            session.setAttribute(USER_SESSION_ATTRIBUTE, user);
+        }
+        return user;
     }
 }
