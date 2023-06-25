fc8b7532dd2a31bbec32030da3c16dc77f1e7e04
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-25 17:26:26 +0300

    add docker compose for creating Mysql container.

---
+++ data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50),
+  age INT
+);
+
+INSERT INTO users (name, age) VALUES ('Batel Peretz', 28);
+INSERT INTO users (name, age) VALUES ('someone random', 33);
